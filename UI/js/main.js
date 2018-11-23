3f16f3456550e5cd82404bbd5de9a3a1ca0f4ee5
ebenezermakinde <ebenezermakinde@yahoo.co.uk>
2018-11-23 21:17:56 +0100

    Worked on Admin portal logo

--- UI/js/main.js
+++ UI/js/main.js
@@ -5,13 +5,11 @@ const closeSideBar = document.getElementById('close-sidebar');
 //function to handle sidebar menu slide open operation.
 const openSlideMenu = () => {
     document.getElementById('side-menu').style.width = '200px';
-    document.getElementById('main').style.marginLeft = '10px';
  }
 
 //function to handle side-bar menu close operation.
 const closeSlideMenu = () => {
     document.getElementById('side-menu').style.width = '0';
-    document.getElementById('main').style.marginLeft = '0';
  }
 
  openSideBar.addEventListener('click', () => {
